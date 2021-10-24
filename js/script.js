19c505a844aa9ee55e3cf1661a78a2433d03e517
saadghonim <86053620+saadghonim@users.noreply.github.com>
2021-10-24 12:59:24 +0200

    edit css

--- js/script.js
+++ js/script.js
@@ -48,21 +48,21 @@ let countDown = new Date('nov 01, 2021 01:00:00').getTime(),
     scaleY: [0.5, 1],
     easing: "easeOutExpo",
     duration: 600,
-    offset: '-=600'
+    // offset: '-=600'
   }).add({
     targets: '.ml5 .letters-left',
     opacity: [0,1],
     translateX: ["0.5em", 0],
     easing: "easeOutExpo",
     duration: 600,
-    offset: '-=300'
+    // offset: '-=300'
   }).add({
     targets: '.ml5 .letters-right',
     opacity: [0,1],
     translateX: ["-0.5em", 0],
     easing: "easeOutExpo",
     duration: 600,
-    offset: '-=600'
+    // offset: '-=600'
   }).add({
     targets: '.ml5',
     opacity: 1,
